6621b074b7d60802288f5f176b0851b70a151354
Andrew Lamb <andrew@nerdnetworks.org>
2020-11-22 08:05:19 -0500

    fixup bench

--- benches/wal.rs
+++ benches/wal.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 use tempfile::TempDir;
-use wal::{Wal, WalOptions};
+use wal::{WalBuilder, WritePayload};
 
 fn wal1(c: &mut Criterion) {
     let mut group = c.benchmark_group("wal");
@@ -10,12 +10,11 @@ fn wal1(c: &mut Criterion) {
 
     group.bench_function("append-1", |b| {
         let dir = TempDir::new().unwrap();
-        let wal = Wal::with_options(
-            dir.path().to_path_buf(),
-            WalOptions::default().sync_writes(false),
-        )
-        .unwrap();
-        b.iter(|| wal.append(&bytes).unwrap())
+        let mut wal = WalBuilder::new(dir.path().to_path_buf()).wal().unwrap();
+        b.iter(|| {
+            let payload = WritePayload::new(bytes.clone()).unwrap();
+            wal.append(payload).unwrap();
+        });
     });
 
     group.finish();
@@ -26,22 +25,18 @@ fn wal10(c: &mut Criterion) {
     let mut group = c.benchmark_group("wal");
     let bytes = vec![1u8; 4096];
 
-    group.throughput(Throughput::Bytes((bytes.len() as u64)*10));
+    group.throughput(Throughput::Bytes(bytes.len() as u64));
 
     group.bench_function("append-10", |b| {
         let dir = TempDir::new().unwrap();
-        let wal = Wal::with_options(
-            dir.path().to_path_buf(),
-            WalOptions::default().sync_writes(false),
-        )
-        .unwrap();
+        let mut wal = WalBuilder::new(dir.path().to_path_buf()).wal().unwrap();
         b.iter(|| {
             for _ in 0..10 {
-                wal.append(&bytes).unwrap();
+                let payload = WritePayload::new(bytes.clone()).unwrap();
+                wal.append(payload).unwrap();
             }
         });
     });
-
 
     group.finish();
 }
@@ -51,25 +46,23 @@ fn wal100(c: &mut Criterion) {
     let mut group = c.benchmark_group("wal");
     let bytes = vec![1u8; 4096];
 
-    group.throughput(Throughput::Bytes((bytes.len() as u64)*100));
+    group.throughput(Throughput::Bytes(bytes.len() as u64));
 
     group.bench_function("append-100", |b| {
         let dir = TempDir::new().unwrap();
-        let wal = Wal::with_options(
-            dir.path().to_path_buf(),
-            WalOptions::default().sync_writes(false),
-        )
-        .unwrap();
+        let mut wal = WalBuilder::new(dir.path().to_path_buf()).wal().unwrap();
         b.iter(|| {
             for _ in 0..100 {
-                wal.append(&bytes).unwrap();
+                let payload = WritePayload::new(bytes.clone()).unwrap();
+                wal.append(payload).unwrap();
             }
         });
     });
 
-
     group.finish();
 }
+
+
 
 
 criterion_group!(benches, wal1, wal10, wal100);
