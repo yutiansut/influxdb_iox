dc52d12dadd670cd3946aae624dced1babedd48c
Paul Dix <paul@pauldix.net>
2020-12-04 19:09:05 -0500

    chore: make file object store only create dir on fail

--- object_store/src/lib.rs
+++ object_store/src/lib.rs
@@ -491,15 +491,25 @@ impl File {
         );
 
         let path = self.path(location);
-        if let Some(parent) = path.parent() {
-            fs::create_dir_all(&parent)
-                .await
-                .context(UnableToCreateDir { path: parent })?;
-        }
 
-        let mut file = fs::File::create(&path)
-            .await
-            .context(UnableToCreateFile { path })?;
+        let mut file = match fs::File::create(&path).await {
+            Ok(f) => f,
+            Err(err) if err.kind() == std::io::ErrorKind::NotFound => match path.parent() {
+                Some(parent) => {
+                    fs::create_dir_all(&parent)
+                        .await
+                        .context(UnableToCreateDir { path: parent })?;
+
+                    match fs::File::create(&path).await {
+                        Ok(f) => f,
+                        Err(err) => return UnableToCreateFile { path, err }.fail(),
+                    }
+                }
+                None => return UnableToCreateFile { path, err }.fail(),
+            },
+            Err(err) => return UnableToCreateFile { path, err }.fail(),
+        };
+
         tokio::io::copy(&mut &content[..], &mut file)
             .await
             .context(UnableToCopyDataToFile)?;
@@ -653,9 +663,9 @@ enum InternalError {
     },
     NoDataInMemory,
 
-    #[snafu(display("Unable to create file {}: {}", path.display(), source))]
+    #[snafu(display("Unable to create file {}: {}", path.display(), err))]
     UnableToCreateFile {
-        source: io::Error,
+        err: io::Error,
         path: PathBuf,
     },
     #[snafu(display("Unable to create dir {}: {}", path.display(), source))]
@@ -899,6 +909,34 @@ mod tests {
                     actual: 11,
                 },
             );
+
+            Ok(())
+        }
+
+        #[tokio::test]
+        async fn creates_dir_if_not_present() -> Result<()> {
+            let root = TempDir::new()?;
+            let storage = ObjectStore::new_file(File::new(root.path()));
+
+            let data = Bytes::from("arbitrary data");
+            let location = "nested/file/test_file";
+
+            let stream_data = std::io::Result::Ok(data.clone());
+            storage
+                .put(
+                    location,
+                    futures::stream::once(async move { stream_data }),
+                    data.len(),
+                )
+                .await?;
+
+            let read_data = storage
+                .get(location)
+                .await?
+                .map_ok(|b| bytes::BytesMut::from(&b[..]))
+                .try_concat()
+                .await?;
+            assert_eq!(&*read_data, data);
 
             Ok(())
         }
